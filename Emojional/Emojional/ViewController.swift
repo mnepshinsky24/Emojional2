959cce833f8c108c213f9b5ab1bcd37114e5ee2d
Megan Nepshinsky <mnepshinsky@gmail.com>
2019-06-06 05:38:21 -0500

    Finished Iteration 1

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton){
+        //When the user clicks the Emoji button
+        // Messages pop up that are encouraging, like, you got this! You can do this! Keep going! We are so proud of you!
+        let alertController = UIAlertController(title: "You're happy", message: "I'm so glad you're happy! Have a great day!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title:"Okay!", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
 
-}
+    }
+
+
 
